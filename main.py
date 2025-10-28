5e529cd0de4beaffc2385c8441984511cf42f296
JoelTobrix <pepedariojoel007@gmail.com>
2025-10-27 19:25:58 -0500

    Tables

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from scripts.data_loader import cargar_catalogo_sismico
 from scripts.data_cleaning import limpiar_datos
 from scripts.data_imputation import imputar_datos
+from scripts.data_visualizacion import graficar_datos
 
 # Ruta del archivo
 ruta_datos = os.path.join("data", "cat_origen_2012-jul2025.txt")
